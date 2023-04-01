da263b3f194b2c6c9794370bb711e26701952552
sakeerin <theblue0800@gmail.com>
2023-04-01 14:17:24 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC  = 10;
+export const RES_PER_PAGE =  10;
+export const KEY = '1aa80643-b20b-4c7e-a59f-63a69a740d9c';
+export const MODAL_CLOSE_SEC = 2.5;
